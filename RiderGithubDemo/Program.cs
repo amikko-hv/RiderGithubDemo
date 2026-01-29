f8029ae526304439578a54072ae2a7a733ef22fd
Andreas Mikko <andreas.mikko@hv.se>
2026-01-29 12:50:35 +0100

    Remove hello

--- RiderGithubDemo/Program.cs
+++ RiderGithubDemo/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, GitHub!");
+        Console.WriteLine("GitHub!");
         Console.WriteLine("A");
         Console.WriteLine("B");
         Console.WriteLine("C");
